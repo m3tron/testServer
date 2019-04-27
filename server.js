7ce167bd2e4cc440611e47d8335f70d5e43c454a
Sunny Heyar <sunny.heyar@gmail.com>
2019-04-27 12:40:54 -0400

    added html

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ var express = require("express");
 
 app = express();
 
-var PORT = 1234;
+var PORT = process.env.PORT || 1234;
+
+app.get("/", function(req, res) {
+  res.json(path.join(__dirname, "public/index.html"));
+});
 
 app.listen(PORT, function() {
   console.log(`Started and running on port: ${PORT}`);
